22a72074ea25ffc8778baa568f88561f100742cf
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-09 17:50:18 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,7 +10,7 @@ class SchoolClassesController < ApplicationController
   
   def create 
     @school_class = SchoolClass.create(title: params[:title], room_number: params[:room_number])
-    binding.pry 
+
     # @school_class = SchoolClass.create(school_class_params)
     redirect_to school_class_path(@school_class)
   end 
