db541542e16f360d414e5a3a7fdf384b080887f8
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-09 17:14:10 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,11 +10,10 @@ class SchoolClassesController < ApplicationController
   
   def show 
     @school_class = SchoolClass.find(params[:id])
-    render 'show.html.erb'
   end
   
   def edit 
-     @school_class = SchoolClass.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
   end 
   
   def update 
