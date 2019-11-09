dfa3f0d75b1e92471fbf2d5e773702ea04fbc01d
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-09 17:15:45 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,9 @@
 class SchoolClassesController < ApplicationController 
   
+  def show 
+    @school_class = SchoolClass.find(params[:id])
+  end
+  
   def new 
     @school_class = SchoolClass.new 
   end 
@@ -8,10 +12,6 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.create(school_class_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end 
-  
-  def show 
-    @school_class = SchoolClass.find(params[:id])
-  end
   
   def edit 
     @school_class = SchoolClass.find(params[:id])
