99a936a216ff478f46628c08158a6d7b94b03f1d
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-09 17:09:16 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,7 +4,7 @@ class SchoolClassesController < ApplicationController
   end 
   
   def create 
-    @school_class = SchoolClass.create(school_class_params)
+    @school_class = SchoolClass.create(school_class_params(:title, :room_number))
   end 
   
   def show 
