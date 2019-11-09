acc003a49ac215b481350ddb4df57c096c2e7c9b
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-09 17:08:40 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,6 +4,7 @@ class SchoolClassesController < ApplicationController
   end 
   
   def create 
+    @school_class = SchoolClass.create(school_class_params)
   end 
   
   def show 
@@ -13,6 +14,12 @@ class SchoolClassesController < ApplicationController
   end 
   
   def update 
+  end 
+  
+  private 
+  
+  def school_class_params(*args) 
+    params.require(:school_class).permit(*args)
   end 
   
 end 
